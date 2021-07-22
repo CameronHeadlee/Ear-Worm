487df07e421a48f235da75363e9974d53f2dd7de
Michael Carmody <mr.carmody89@gmail.com>
2021-07-22 11:06:08 -0400

    Correct MM API, variables, SP HTML id added

--- js/searchResults.js
+++ js/searchResults.js
@@ -1,8 +1,11 @@
 
 //GLOBAL VARIABLES 
 var player;
+var resultContent = document.querySelector("#livesearch")
+var musicsearch = document.querySelector('#musicsearch');
 
-//FUNCTIONS 
+//FUNCTIONS
+var YTP = "https://www.youtube.com/iframe_api"; 
 function onYouTubeIframeAPIReady() {
   player = new YT.Player('player', {
     height: '390',
@@ -18,32 +21,44 @@ function onYouTubeIframeAPIReady() {
   });
 }
 
-//CALLS 
 function onPlayerReady(event) {
+    event.target.playVideo();
+  }
+
+
+  function onPlayerReady(event) {
     event.target.playVideo();
   }
   
 
-var musicsearch = document.querySelector('#musicsearch');
-
 function MusicSearch(query) {
-    var locMMURL = 'https://www.musixmatch.com/search/?fo=json';
-
-    locMMURL = locMMURL + '&q=' + query;
+    var locMMURL = 'https://api.musixmatch.com/ws/1.1/search/?q=' + query;
 
     fetch(locMMURL)
     .then(function (response) {
       if(!response.ok) {
         throw response.json();
       }
+
       return response.json();
     })
     .then(function (locRes) {
+      resultText.textContent = locRes.search.query;
 
+      console.log(locRes);
+
+      if(!locRes.results.length) {
+        console.log('No results found!');
+        resultContent.textContent = '';
+        for (var i = 0; i < locRes.results.length; i++) {
+          printResults(locRes.resluts[i]);
+        }
+      }
     })
+    .catch(function (error) {
+      console.error(error);
+    });
 }
-
-
 
 function MusicSearchSubmit(event) {
     event.preventDefault();
@@ -60,11 +75,3 @@ function MusicSearchSubmit(event) {
 }
 
 musicsearch.addEventListener('submit', MusicSearchSubmit);
-
-
-// NOTES FOR A STEP. PLEASE DO NOT REMOVE. 
-// THIS WILL BE NEEDED TO TAKE RESULTS TO YOUTUBE API.
-// var linkButtonEl = document.createElement('a');
-// linkButtonEl.textContent = 'Read More';
-// linkButtonEl.setAttribute('href', resultObj.url);
-// linkButtonEl.classList.add('btn', 'btn-dark');
